fd58f91d0a483cb531554821e264e1c92ea61bc1
Allison Walke <awalke796@gmail.com>
2018-10-14 21:26:45 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+$(document).ready(function (){
+  setTimeout(function () {
+        $(".dark-box").css("background-color", "rgba(0,0,0,0.6)");
+        $(".title h1").css("color", "white");
+
+        setTimeout(function () {
+          $(".title img").css("opacity", "0.5");
+
+          setTimeout(function () {
+            $(".title img").css("-webkit-transition", "opacity 0s");
+            $(".title img").css("transition", "opacity 0s");
+
+            $(".title img").effect( "bounce", {times:3}, 1500 );
+          }, 1000);
+        }, 1000);
+    }, 500);
+
+    $(".title img").hover(function(){
+        $(this).css("opacity", "1");
+        }, function(){
+        $(this).css("opacity", "0.5");
+    });
+
+    $(".title img").click(function() {
+      $('html, body').animate({
+        scrollTop: $(".navigation-page").offset().top
+    }, 1000);
+    });
+
+    $("#center").click(function() {
+      window.location.href = "test.html";
+    });
+});
